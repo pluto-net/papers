d1123f411025999aaea55b518b8dba2343ac48f2
Tylor Shin <shincode@icloud.com>
2017-12-13 20:13:21 +0900

    Add basic shavejs logic

--- imports/components/home/components/icoCard.tsx
+++ imports/components/home/components/icoCard.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as moment from "moment";
 import { Link } from "react-router-dom";
 import { Rating, Grid } from "semantic-ui-react";
+const shave = require("shave");
 import { IPost } from "../../../../both/model/post";
 import { getWhitePaperAddress } from "../../../helpers/getWhitepaperAddress";
 
@@ -10,97 +11,114 @@ interface IICOCardProps {
   type: string;
 }
 
-const ICOCard = (props: IICOCardProps) => {
-  const { post } = props;
-  const rating = Math.round(post.averageRating);
-  const avgRating = post.averageRating.toFixed(1);
-  const fromNow = moment(post.endICODate).fromNow();
-  let content: string;
-  if (post.content && post.content.length > 50) {
-    content = post.content.slice(0, 50) + "...";
-  } else if (!post.content) {
-    content = "There is no description yet";
-  } else {
-    content = post.content;
+class ICOCard extends React.Component<IICOCardProps, {}> {
+  private contentNode: HTMLDivElement | null;
+
+  public componentDidMount() {
+    shave(this.contentNode, 91);
   }
 
-  if (!post) return null;
+  public render() {
+    const { post } = this.props;
+    const rating = Math.round(post.averageRating);
+    const avgRating = post.averageRating.toFixed(1);
+    const fromNow = moment(post.endICODate).fromNow();
+    let content: string;
+    if (post.content && post.content.length > 50) {
+      content = post.content.slice(0, 50) + "...";
+    } else if (!post.content) {
+      content = "There is no description yet";
+    } else {
+      content = post.content;
+    }
 
-  return (
-    <Grid.Column>
-      <div className="ico-card-item-wrapper">
-        <div className="ico-card-item-title-box">
-          <div className="ico-card-item-title-left-box">
-            <div className="ico-card-item-img-wrapper">
-              <img src={post.logoUrl} alt={post.title} />
+    if (!post) return null;
+
+    console.log(shave);
+
+    return (
+      <Grid.Column>
+        <div className="ico-card-item-wrapper">
+          <div className="ico-card-item-title-box">
+            <div className="ico-card-item-title-left-box">
+              <div className="ico-card-item-img-wrapper">
+                <img src={post.logoUrl} alt={post.title} />
+              </div>
+            </div>
+            <div className="ico-card-item-title-right-box">
+              <div className="ico-card-item-title">
+                <Link
+                  to={{
+                    pathname: `/posts/${post._id}`,
+                    state: { modal: true },
+                  }}
+                  className="ico-card-item-modal-link"
+                >
+                  {post.title}
+                </Link>
+              </div>
+              <div className="ico-card-item-rating-box">
+                <Rating style={{ marginRight: 3 }} icon="star" maxRating={5} rating={rating} disabled />
+                <span className="average-rating">{avgRating}</span>
+                <span className="average-count">{` (${post.ratingCount} participants)`}</span>
+              </div>
+              <div className="field-wrapper">{post.fields ? post.fields.join(" · ") : null}</div>
             </div>
           </div>
-          <div className="ico-card-item-title-right-box">
-            <div className="ico-card-item-title">
-              <Link
-                to={{
-                  pathname: `/posts/${post._id}`,
-                  state: { modal: true },
+          <div className="content-wrapper">
+            <Link
+              to={{
+                pathname: `/posts/${post._id}`,
+                state: { modal: true },
+              }}
+              style={{ color: "inherit" }}
+              className="ico-card-item-modal-link"
+            >
+              <div
+                style={{ lineHeight: "22.5px" }}
+                ref={el => {
+                  this.contentNode = el;
                 }}
-                className="ico-card-item-modal-link"
               >
-                {post.title}
-              </Link>
-            </div>
-            <div className="ico-card-item-rating-box">
-              <Rating style={{ marginRight: 3 }} icon="star" maxRating={5} rating={rating} disabled />
-              <span className="average-rating">{avgRating}</span>
-              <span className="average-count">{` (${post.ratingCount} participants)`}</span>
-            </div>
-            <div className="field-wrapper">{post.fields ? post.fields.join(" · ") : null}</div>
+                {content}
+              </div>
+            </Link>
+          </div>
+
+          <div className="link-buttons-wrapper">
+            <a target="_blank" href={getWhitePaperAddress(post)} className="link-button">
+              White Paper
+            </a>
+            <a target="_blank" href={post.links ? post.links[0] : getWhitePaperAddress(post)} className="link-button">
+              Homepage
+            </a>
+          </div>
+
+          <div className="meta-information-wrapper">
+            <Link
+              to={{
+                pathname: `/posts/${post._id}`,
+                state: { modal: true },
+              }}
+              className="ico-card-item-modal-link fromNow"
+            >
+              {fromNow}
+            </Link>
+            <Link
+              to={{
+                pathname: `/posts/${post._id}`,
+                state: { modal: true },
+              }}
+              className="comment-info"
+            >
+              Comments
+              <span className="comment-count">{` ${post.commentCount}`}</span>
+            </Link>
           </div>
         </div>
-        <div className="content-wrapper">
-          <Link
-            to={{
-              pathname: `/posts/${post._id}`,
-              state: { modal: true },
-            }}
-            style={{ color: "inherit" }}
-            className="ico-card-item-modal-link"
-          >
-            {content}
-          </Link>
-        </div>
-
-        <div className="link-buttons-wrapper">
-          <a target="_blank" href={getWhitePaperAddress(post)} className="link-button">
-            White Paper
-          </a>
-          <a target="_blank" href={post.links ? post.links[0] : getWhitePaperAddress(post)} className="link-button">
-            Homepage
-          </a>
-        </div>
-
-        <div className="meta-information-wrapper">
-          <Link
-            to={{
-              pathname: `/posts/${post._id}`,
-              state: { modal: true },
-            }}
-            className="ico-card-item-modal-link fromNow"
-          >
-            {fromNow}
-          </Link>
-          <Link
-            to={{
-              pathname: `/posts/${post._id}`,
-              state: { modal: true },
-            }}
-            className="comment-info"
-          >
-            Comments
-            <span className="comment-count">{` ${post.commentCount}`}</span>
-          </Link>
-        </div>
-      </div>
-    </Grid.Column>
-  );
-};
+      </Grid.Column>
+    );
+  }
+}
 
 export default ICOCard;
